7b7ff63db4d6b0a2ab0edefc8e1d2d75fdccfc0b
amine khatmi <a.khatmi@upm.ac.ma7>
2022-04-27 00:43:58 +0000

    examens table

--- app/Http/Controllers/administrateur/ExamenController.php
+++ app/Http/Controllers/administrateur/ExamenController.php
@@ -8,4 +8,47 @@ use Illuminate\Http\Request;
 class ExamenController extends Controller
 {
     //
+    public function insert(Request $req){
+        $examen = new Examen;
+        $examen->date=$req->input("date");
+        $examen->heure=$req->input("heure");
+        $examen->idmatiere=$req->input("idmatiere");
+        $examen->idsalle=$req->input("idsalle");
+        if($examen->save()){
+                return response()->json([
+                    "message" => "succés"
+                ]);
+            }
+
+        else
+        return response()->json(["message" =>"erreur"]);
+
+    }
+    public function getAll(){
+        $examen = Examen::All()->get();
+        return $examen;
+    }
+    public function update(Request $req){
+
+        if(Examen::where('id','=',$req->input("id"))->count()>0){
+            $examen=Examen::where('id','=',$req->input("id"))->update(['date'=>$req->input("date"),'heure'=>$req->input("heure"),'etagesalle'=>$req->input("etagesalle"),'typesalle'=>$req->input("typesalle"),'nombreplaces'=>$req->input("nombreplaces")]);
+                return response()->json([
+                    "message" => "succés"
+                ]);
+        }
+        else
+        return response()->json(["message" =>"Nom existe"]);
+    }
+    public function delete(Request $req){
+
+        if((Examen::where('id','=',$req->input("id"))->count()>0)){
+            $examen=Examen::where('id','=',$req->input("id"))->delete();
+                return response()->json([
+                    "message" => "succés"
+                ]);
+        }
+        else
+        return response()->json(["message" =>"il n'y a pas cette enregistrement"]);
+    }
+
 }
--- app/Http/Controllers/administrateur/SalleController.php
+++ app/Http/Controllers/administrateur/SalleController.php
@@ -8,4 +8,48 @@ use Illuminate\Http\Request;
 class SalleController extends Controller
 {
     //
+
+    public function insert(Request $req){
+        $salle = new Salle;
+        $salle->nomsalle=$req->input("nomsalle");
+        $salle->etagesalle=$req->input("etagesalle");
+        $salle->typesalle=$req->input("typesalle");
+        $salle->nombreplaces=$req->input("nombreplaces");
+        if(!(Salle::where('nomsalle','=',$req->input("nomsalle"))->count()>0)){
+            if($salle->save()){
+                return response()->json([
+                    "message" => "succés"
+                ]);
+            }
+        }
+        else
+        return response()->json(["message" =>"Nom existe"]);
+
+    }
+    public function getAll(){
+        $salle = Salle::All()->get();
+        return $salle;
+    }
+    public function update(Request $req){
+
+        if(!(Salle::where('nomsalle','=',$req->input("nomsalle"))->count()>0) && Salle::where('id','=',$req->input("id"))->count()>0){
+            $salle=Salle::where('id','=',$req->input("id"))->update(['nomsalle'=>$req->input("nomsalle"),'etagesalle'=>$req->input("etagesalle"),'typesalle'=>$req->input("typesalle"),'nombreplaces'=>$req->input("nombreplaces")]);
+                return response()->json([
+                    "message" => "succés"
+                ]);
+        }
+        else
+        return response()->json(["message" =>"Nom existe"]);
+    }
+    public function delete(Request $req){
+
+        if((Salle::where('id','=',$req->input("id"))->count()>0)){
+            $salle=Salle::where('id','=',$req->input("id"))->delete();
+                return response()->json([
+                    "message" => "succés"
+                ]);
+        }
+        else
+        return response()->json(["message" =>"il n'y a pas cette enregistrement"]);
+    }
 }
